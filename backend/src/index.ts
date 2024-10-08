8673af2004c6ddfc971acb4b3987961784204df3
Tim Grünewald <tim.gruenewald@tutamail.com>
2024-10-08 10:19:24 +0200

    feat: Integrate TypeScript, .env, closes #9

---
+++ backend/src/index.ts
@@ -0,0 +1,21 @@
+// src/index.ts
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+
+const app = express();
+const port = process.env.PORT || 3333;
+
+// Middleware to parse JSON requests
+app.use(express.json());
+
+// Define a basic route
+app.get("/", (req, res) => {
+  res.send("Hello, TypeScript with Express!");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
