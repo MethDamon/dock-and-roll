2b2ca43c387b1f9cecb8fd3bac43ffa18fa8e41b
Tim Grünewald <tim.gruenewald@tutamail.com>
2024-10-09 17:20:17 +0200

    feat: Integrate Clerk

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,117 +1,110 @@
-"use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const dotenv_1 = __importDefault(require("dotenv"));
-dotenv_1.default.config();
-const express_1 = __importDefault(require("express"));
-const client_1 = require("@libsql/client");
-const libsql_1 = require("drizzle-orm/libsql");
-const schema = __importStar(require("./schema.js"));
-const expressions_1 = require("drizzle-orm/sqlite-core/expressions");
-const dbClient = (0, client_1.createClient)({
+import dotenv from "dotenv";
+dotenv.config();
+import express, {} from "express";
+import cors from "cors";
+import { createClient } from "@libsql/client";
+import { drizzle } from "drizzle-orm/libsql";
+import * as schema from "./schema.js";
+import { eq } from "drizzle-orm/sqlite-core/expressions";
+import { ClerkExpressRequireAuth, ClerkExpressWithAuth, } from "@clerk/clerk-sdk-node";
+const dbClient = createClient({
     url: process.env.DATABASE_URL || "file:local.db",
 });
-const db = (0, libsql_1.drizzle)(dbClient, { schema });
+const db = drizzle(dbClient, { schema });
 const PORT = process.env.PORT || 3000;
-const app = (0, express_1.default)();
-app.use(express_1.default.json()); // Middleware to parse JSON request bodies
+const app = express();
+app.use(express.json()); // Middleware to parse JSON request bodies
+app.use(cors());
 // CREATE
-app.post("/boats", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.post("/boats", ClerkExpressWithAuth(), async (req, res) => {
     if (req.body) {
         const newBoat = req.body; // Assume the new item is sent in the request body
-        const result = yield db.insert(schema.boats).values(newBoat).returning();
+        const result = await db.insert(schema.boats).values(newBoat).returning();
         res.status(201).json(result);
     }
     else {
         res.status(400).json({ message: "Missing request body" });
     }
-}));
+});
 // READ (Get all boats)
-app.get("/boats", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const boats = yield db.select().from(schema.boats);
+app.get("/boats", ClerkExpressRequireAuth(), async (req, res) => {
+    const boats = await db.select().from(schema.boats);
     res.json(boats);
-}));
+});
 // READ (Get a single item by ID)
-app.get("/boats/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const id = parseInt(req.params.id);
-    const item = yield db.query.boats.findFirst({
-        where: (0, expressions_1.eq)(schema.boats.id, id),
-    });
-    if (!item) {
-        res.status(404).json({ message: "Boat not found" });
+app.get("/boats/:id", ClerkExpressRequireAuth(), async (req, res) => {
+    if (!req.params.id) {
+        res.status(400).json({ message: "No id provided" });
         return;
     }
-    res.json(item);
-}));
+    else {
+        const id = parseInt(req.params.id);
+        const item = await db.query.boats.findFirst({
+            where: eq(schema.boats.id, id),
+        });
+        if (!item) {
+            res.status(404).json({ message: "Boat not found" });
+            return;
+        }
+        res.json(item);
+    }
+});
 // UPDATE
-app.put("/boats/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.put("/boats/:id", ClerkExpressRequireAuth(), async (req, res) => {
     const newBoat = req.body;
-    const id = parseInt(req.params.id);
-    const result = yield db.query.boats.findFirst({
-        where: (0, expressions_1.eq)(schema.boats.id, id),
-    });
-    if (!result) {
-        res.status(404).json({ message: "Boat not found" });
+    if (!req.params.id) {
+        res.status(400).json({ message: "No id provided" });
         return;
     }
     else {
-        const dbResult = yield db
-            .update(schema.boats)
-            .set(newBoat)
-            .where((0, expressions_1.eq)(schema.boats.id, id))
-            .returning();
-        res.json(dbResult);
+        const id = parseInt(req.params.id);
+        const result = await db.query.boats.findFirst({
+            where: eq(schema.boats.id, id),
+        });
+        if (!result) {
+            res.status(404).json({ message: "Boat not found" });
+            return;
+        }
+        else {
+            const dbResult = await db
+                .update(schema.boats)
+                .set(newBoat)
+                .where(eq(schema.boats.id, id))
+                .returning();
+            res.json(dbResult);
+        }
     }
-}));
+});
 // DELETE
-app.delete("/boats/:id", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const id = parseInt(req.params.id);
-    const result = yield db.query.boats.findFirst({
-        where: (0, expressions_1.eq)(schema.boats.id, id),
-    });
-    if (!result) {
-        res.status(404).json({ message: "Boat not found" });
+app.delete("/boats/:id", ClerkExpressRequireAuth(), async (req, res) => {
+    if (!req.params.id) {
+        res.status(400).json({ message: "No id provided" });
         return;
     }
     else {
-        yield db.delete(schema.boats).where((0, expressions_1.eq)(schema.boats.id, id)).returning();
-        res.status(204);
+        const id = parseInt(req.params.id);
+        const result = await db.query.boats.findFirst({
+            where: eq(schema.boats.id, id),
+        });
+        if (!result) {
+            res.status(404).json({ message: "Boat not found" });
+            return;
+        }
+        else {
+            await db
+                .delete(schema.boats)
+                .where(eq(schema.boats.id, id))
+                .returning();
+            res.status(204);
+        }
     }
-}));
+});
+const errorHandler = (err, _, res, next) => {
+    console.error(err.stack);
+    res.status(401).send({ message: "Unauthenticated" });
+    next();
+};
+app.use(errorHandler);
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
--- backend/src/interfaces.ts
+++ backend/src/interfaces.ts
@@ -1,41 +1,13 @@
-import { type Request, type Response } from "express";
-import type { Boat } from "./types.js";
+import {
+  type Response as ExpressResponse,
+  type Request as ExpressRequest,
+} from "express";
+import { type Send } from "express-serve-static-core";
 
-export interface BoatCreateRequest extends Request {
-  body: Boat;
+export interface TypedRequest<T> extends ExpressRequest {
+  body: T;
 }
 
-export interface BoatCreateResponse extends Response {
-  body?: Boat;
-}
-
-export interface BoatGetDetailRequest extends Request {
-  params: {
-    id: string;
-  };
-}
-
-export interface BoatGetDetailResponse extends Response {
-  body?: Boat | Error;
-}
-
-export interface BoatUpdateRequest extends Request {
-  params: {
-    id: string;
-  };
-  body: Omit<Boat, "id">;
-}
-
-export interface BoatUpdateResponse extends Response {
-  body?: Boat | Error;
-}
-
-export interface BoatDeleteRequest extends Request {
-  params: {
-    id: string;
-  };
-}
-
-export interface BoatDeleteResponse extends Response {
-  body?: Boat | Error;
+export interface TypedResponse<T> extends ExpressResponse {
+  json: Send<T, this>;
 }
