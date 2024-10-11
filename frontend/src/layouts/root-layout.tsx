4d72b85642eab728a46710ebea225b89b3f17a2d
Tim Grünewald <tim.gruenewald@tutamail.com>
2024-10-11 11:41:12 +0200

    feat: Delete boat funcitonality

--- frontend/src/layouts/root-layout.tsx
+++ frontend/src/layouts/root-layout.tsx
@@ -28,9 +28,9 @@ export const RootLayout = () => (
       <SignedIn>
         <div className="flex min-h-screen w-full flex-col">
           <SideNavigation />
-          <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+          <div className="flex flex-1 flex-col sm:gap-4 sm:py-4 sm:pl-14">
             <Header />
-            <main className="grid flex-1 items-start gap-4 p-6 sm:px-6 sm:py-0 md:gap-4">
+            <main className="grid items-start gap-4 p-6 sm:px-6 sm:py-0 md:gap-4">
               <Breadcrumbs />
               <Outlet />
             </main>
